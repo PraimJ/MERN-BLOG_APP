e6208be30b596f137a9e0262b7d786f2fe1ef6bf
Praim Jutla <praimjutla@Praims-MacBook-Pro.local>
2023-03-16 13:01:56 -0400

    Making Routes, Header and Naviagtion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,31 @@
+import Header from "./components/Header";
+import React, {Fragment } from "react";
+import {Route, Routes } from "react-router-dom"
+
+import AddBlogs from "./components/AddBlogs";
+import Auth from "./components/Auth";
+import Blogs from "./components/Blogs";
+import UserBlogs from "./components/UserBlogs";
+import BlogDetail from "./components/BlogDetail";
 
 
 function App() {
   return (
-    <div>
-      hello world
-    </div>
-  );
-}
+  <Fragment>
+   < header>
+   <Header/>
+   </header>
+  <main>
+    <Routes>
+<Route path ="/auth" element={<Auth/>}/>
+<Route path ="/blogs" element={<Blogs/>}/>
+<Route path ="/blogs/add" element={<AddBlogs/>}/>
+<Route path ="/myBlogs" element={<UserBlogs/>}/>
+<Route path ="/myBlogs/:id" element={<BlogDetail/>}/>
+ </Routes>
+  </main>
+  </Fragment>
+  
+  )};
 
 export default App;
